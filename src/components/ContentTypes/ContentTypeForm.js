7c1fa8d44f2f045bb60d86ab551898b0b346c30b
Jamena McInteer <jamena@webdevabq.com>
2018-11-04 21:09:39 -0600

    Changed ContentTypeForm to functional component with Hooks, created custom useSlugify hook

--- src/components/ContentTypes/ContentTypeForm.js
+++ src/components/ContentTypes/ContentTypeForm.js
@@ -1,118 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import useSlugify from "../../hooks/useSlugify";
 
-export default class ContentTypeForm extends React.Component {
-  constructor(props) {
-    super(props);
+export const ContentTypeForm = props => {
+  const [title, setTitle] = useState(
+    props.contentType ? props.contentType.title : ""
+  );
+  const [slug, setSlug] = useState(
+    props.contentType ? props.contentType.slug : ""
+  );
+  const [fields, setFields] = useState(
+    props.contentType ? props.contentType.fields : []
+  );
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const slugify = useSlugify(null);
 
-    this.state = {
-      title: props.contentType ? props.contentType.title : "",
-      slug: props.contentType ? props.contentType.slug : "",
-      fields: props.contentType ? props.contentType.fields : [],
-      error: ""
-    };
-  }
-  useSlugify = string => {
-    const a = "àáäâãåèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;";
-    const b = "aaaaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------";
-    const p = new RegExp(a.split("").join("|"), "g");
-    return string
-      .toString()
-      .toLowerCase()
-      .replace(/\s+/g, "-") // Replace spaces with
-      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-      .replace(/&/g, "-and-") // Replace & with ‘and’
-      .replace(/[^\w\-]+/g, "") // Remove all non-word characters
-      .replace(/\-\-+/g, "-") // Replace multiple — with single -
-      .replace(/^-+/, ""); // Trim — from start of text .replace(/-+$/, '') // Trim — from end of text
+  const onTitleChange = e => {
+    setTitle(e.target.value);
+    setSlug(useSlugify(e.target.value));
   };
-  onTitleChange = e => {
-    const title = e.target.value;
-    const slug = this.useSlugify(title);
-    this.setState(() => ({
-      title,
-      slug
-    }));
-  };
-  onSlugChange = e => {
-    const slug = e.target.value;
-    this.setState(() => ({
-      slug
-    }));
-  };
-  onFieldChange = e => {
+  const onFieldChange = e => {
     const field = e.target.checked ? e.target.value : null;
-    let fields = this.state.fields ? this.state.fields : [];
-    let newFields = fields;
+    let oldFields = fields ? fields : [];
+    let newFields = oldFields;
 
     if (field) {
       newFields.push(field);
     } else {
-      newFields = fields.filter(function(value, index, arr) {
+      newFields = oldFields.filter(function(value, index, arr) {
         return value !== e.target.value;
       });
     }
-    this.setState(() => ({
-      fields: newFields
-    }));
+    setFields(newFields);
   };
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
     // TO DO: Check that slug is unique in db
-    if (!this.state.title || !this.state.slug) {
+    if (!title || !slug) {
       const error = "Please provide title and slug";
       const success = "";
-      this.setState(() => ({ error, success }));
+      setError(error);
+      setSuccess(success);
     } else {
       const error = "";
       const success = "Content type saved successfully.";
-      this.setState(() => ({ error, success }));
-      this.props.onSubmit({
-        title: this.state.title,
-        slug: this.state.slug,
-        fields: this.state.fields
+      setError(error);
+      setSuccess(success);
+      props.onSubmit({
+        title: title,
+        slug: slug,
+        fields: fields
       });
     }
   };
-  render() {
-    return (
-      <form className="form" onSubmit={this.onSubmit}>
-        {this.state.error && <p className="form__error">{this.state.error}</p>}
-        {this.state.success && (
-          <p className="form__success">{this.state.success}</p>
-        )}
-        <input
-          className="text-input"
-          type="text"
-          placeholder="Title"
-          autoFocus
-          value={this.state.title}
-          onChange={this.onTitleChange}
-        />
-        <input
-          className="text-input"
-          type="text"
-          placeholder="Slug"
-          value={this.state.slug}
-          onChange={this.onSlugChange}
-        />
-        {this.props.fields &&
-          this.props.fields.map(field => {
-            return (
-              <div key={field.id}>
-                <input
-                  type="checkbox"
-                  value={field.slug}
-                  onChange={this.onFieldChange}
-                  checked={this.state.fields.includes(field.slug)}
-                />{" "}
-                {field.name}
-              </div>
-            );
-          })}
-        <div>
-          <button className="button">Save Content Type</button>
-        </div>
-      </form>
-    );
-  }
-}
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      {error && <p className="form__error">{error}</p>}
+      {success && <p className="form__success">{success}</p>}
+      <input
+        className="text-input"
+        type="text"
+        placeholder="Title"
+        autoFocus
+        value={title}
+        onChange={onTitleChange}
+      />
+      <input
+        className="text-input"
+        type="text"
+        placeholder="Slug"
+        value={slug}
+        onChange={e => setSlug(e.target.value)}
+      />
+      {props.fields &&
+        props.fields.map(field => {
+          return (
+            <div key={field.id}>
+              <input
+                type="checkbox"
+                value={field.slug}
+                onChange={onFieldChange}
+                checked={fields.includes(field.slug)}
+              />{" "}
+              {field.name}
+            </div>
+          );
+        })}
+      <div>
+        <button className="button">Save Content Type</button>
+      </div>
+    </form>
+  );
+};
+
+export default ContentTypeForm;
--- src/components/SettingsForm.js
+++ src/components/SettingsForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 export const SettingsForm = props => {
   const [siteTitle, setSiteTitle] = useState(
