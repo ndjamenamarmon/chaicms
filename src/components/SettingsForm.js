b9e8a71dfdea13cc68c4baf2f6876c13428c3fb0
Jamena McInteer <jamena@webdevabq.com>
2018-11-04 20:50:29 -0600

    Updated react-dom to 16.7.0 to match react, changed SettingsForm to function component with Hooks

--- src/components/SettingsForm.js
+++ src/components/SettingsForm.js
@@ -1,69 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default class SettingsForm extends React.Component {
-  constructor(props) {
-    super(props);
+export const SettingsForm = props => {
+  const [siteTitle, setSiteTitle] = useState(
+    props.settings ? props.settings.siteTitle : ""
+  );
+  const [siteDescription, setSiteDescription] = useState(
+    props.settings ? props.settings.siteDescription : ""
+  );
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
-    this.state = {
-      siteTitle: props.settings ? props.settings.siteTitle : "",
-      siteDescription: props.settings ? props.settings.siteDescription : ""
-    };
-  }
-  onSiteTitleChange = e => {
-    const siteTitle = e.target.value;
-    this.setState(() => ({
-      siteTitle
-    }));
-  };
-  onSiteDescriptionChange = e => {
-    const siteDescription = e.target.value;
-    this.setState(() => ({
-      siteDescription
-    }));
-  };
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
 
-    if (!this.state.siteTitle) {
+    if (!siteTitle) {
       const error = "Please provide site title";
       const success = "";
-      this.setState(() => ({ error, success }));
+      setError(error);
+      setSuccess(success);
     } else {
       const error = "";
       const success = "Settings saved successfully.";
-      this.setState(() => ({ error, success }));
-      this.props.onSubmit({
-        siteTitle: this.state.siteTitle,
-        siteDescription: this.state.siteDescription
+      setError(error);
+      setSuccess(success);
+      props.onSubmit({
+        siteTitle,
+        siteDescription
       });
     }
   };
-  render() {
-    return (
-      <form className="form" onSubmit={this.onSubmit}>
-        {this.state.error && <p className="form__error">{this.state.error}</p>}
-        {this.state.success && (
-          <p className="form__success">{this.state.success}</p>
-        )}
-        <input
-          className="text-input"
-          type="text"
-          placeholder="Site title"
-          autoFocus
-          value={this.state.siteTitle}
-          onChange={this.onSiteTitleChange}
-        />
-        <input
-          className="text-input"
-          type="text"
-          placeholder="Site description"
-          value={this.state.siteDescription}
-          onChange={this.onSiteDescriptionChange}
-        />
-        <div>
-          <button className="button">Save Settings</button>
-        </div>
-      </form>
-    );
-  }
-}
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      {error && <p className="form__error">{error}</p>}
+      {success && <p className="form__success">{success}</p>}
+      <input
+        className="text-input"
+        type="text"
+        placeholder="Site title"
+        autoFocus
+        value={siteTitle}
+        onChange={e => setSiteTitle(e.target.value)}
+      />
+      <input
+        className="text-input"
+        type="text"
+        placeholder="Site description"
+        value={siteDescription}
+        onChange={e => setSiteDescription(e.target.value)}
+      />
+      <div>
+        <button className="button">Save Settings</button>
+      </div>
+    </form>
+  );
+  // }
+};
+
+export default SettingsForm;
