3e58da76eeed8f400382026bb0e206231e49254a
Jamena McInteer <jamena@webdevabq.com>
2018-11-10 06:58:31 -0600

    Updated field list: display and styling, alphabetical sort

--- src/components/Fields/FieldListItem.js
+++ src/components/Fields/FieldListItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const FieldListItem = ({ id, name, slug, type }) => (
+const FieldListItem = ({ id, name, display, type }) => (
   <Link className="list-item" to={`/fields/edit/${id}`}>
     <div>
       <h3 className="list-item__title">{name}</h3>
-      <span className="list-item__sub-title">{slug}</span>
     </div>
-    <h3 className="list-item__data">{type}</h3>
+    <div>
+      <h3 className="list-item__data">{type}</h3>
+      <span className="list-item__sub-data">{display}</span>
+    </div>
   </Link>
 );
 
--- src/selectors/fields.js
+++ src/selectors/fields.js
@@ -1,6 +1,6 @@
 // Get visible fields
 export default fields => {
   return fields.sort((a, b) => {
-    return a.title < b.title ? 1 : -1;
+    return a.name > b.name ? 1 : -1;
   });
 };
