db83d4260bc94c6951e6d64df2b51c2f66b7342e
Jamena McInteer <jamena@webdevabq.com>
2018-11-30 08:33:06 -0600

    Expose more settings in redux store if user is admin or owner

--- src/actions/settings.js
+++ src/actions/settings.js
@@ -26,15 +26,44 @@ export const setSettings = settings => ({
 
 export const startSetSettings = () => {
   return (dispatch, getState) => {
+    // need to populate with more settings based on role; admin and owner can see everything
     return database
-      .ref("settings")
+      .ref(`users`)
       .once("value")
       .then(snapshot => {
-        const settings = {
-          siteDescription: snapshot.val().siteDescription,
-          siteTitle: snapshot.val().siteTitle
-        };
-        dispatch(setSettings(settings));
+        snapshot.forEach(childSnapshot => {
+          if (childSnapshot.val().uid === getState().auth.uid) {
+            const roleId = childSnapshot.val().roleId;
+
+            return database
+              .ref(`user_roles/${roleId}`)
+              .once("value")
+              .then(snapshot => {
+                const role = snapshot.val().name;
+
+                if (role === "admin" || role === "owner") {
+                  return database
+                    .ref("settings")
+                    .once("value")
+                    .then(snapshot => {
+                      const settings = snapshot.val();
+                      dispatch(setSettings(settings));
+                    });
+                } else {
+                  return database
+                    .ref("settings")
+                    .once("value")
+                    .then(snapshot => {
+                      const settings = {
+                        siteDescription: snapshot.val().siteDescription,
+                        siteTitle: snapshot.val().siteTitle
+                      };
+                      dispatch(setSettings(settings));
+                    });
+                }
+              });
+          }
+        });
       });
   };
 };
