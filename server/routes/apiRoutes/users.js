ed3198f2126cead1097220f51d4dee69453414c2
Jamena McInteer <jamena@webdevabq.com>
2019-05-22 08:42:40 -0500

    Added required permissions to api routes for users

--- server/routes/apiRoutes/users.js
+++ server/routes/apiRoutes/users.js
@@ -1,6 +1,7 @@
 // const passport = require("passport");
 const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
+const requirePermissions = require("../middleware/requirePermissions");
 
 const User = mongoose.model("users");
 
@@ -25,54 +26,64 @@ module.exports = app => {
   //   res.send(req.body);
   // });
 
-  app.put("/api/users/:id", requireLogin, (req, res) => {
-    // res.send(req.params.id);
+  app.put(
+    "/api/users/:id",
+    requireLogin,
+    requirePermissions(["UPDATE_USERS"]),
+    (req, res) => {
+      // res.send(req.params.id);
 
-    // User.findOne({ _id: req.params.id }).then(user => {
-    //   if (user) {
-    //     // we have a record, let's update it
-    //     done(null, existingUser); // first argument is error
-    //   } else {
-    //     // we don't have a user record with this ID, give an error
-    //     new User({
-    //       googleId: profile.id,
-    //       displayName: profile.displayName,
-    //       email: profile.email,
-    //       isApproved: true,
-    //       photoURL: profile.photoURL
-    //     })
-    //       .save()
-    //       .then(user => done(null, user)); // new instance of a user, then save to db
+      // User.findOne({ _id: req.params.id }).then(user => {
+      //   if (user) {
+      //     // we have a record, let's update it
+      //     done(null, existingUser); // first argument is error
+      //   } else {
+      //     // we don't have a user record with this ID, give an error
+      //     new User({
+      //       googleId: profile.id,
+      //       displayName: profile.displayName,
+      //       email: profile.email,
+      //       isApproved: true,
+      //       photoURL: profile.photoURL
+      //     })
+      //       .save()
+      //       .then(user => done(null, user)); // new instance of a user, then save to db
 
-    //     done(null, user);
-    //   }
-    // });
+      //     done(null, user);
+      //   }
+      // });
 
-    let update = new User({ _id: req.params.id, ...req.body });
-    update.isNew = false;
-    update.save(err => {
-      if (err) res.send(err);
-      else {
-        res.send({ message: "ok" });
-      }
-    });
-    // .then(user => {
-    //   done(null, user);
-    //   res.send({ message: "success" });
-    // });
+      let update = new User({ _id: req.params.id, ...req.body });
+      update.isNew = false;
+      update.save(err => {
+        if (err) res.send(err);
+        else {
+          res.send({ message: "ok" });
+        }
+      });
+      // .then(user => {
+      //   done(null, user);
+      //   res.send({ message: "success" });
+      // });
 
-    // res.send(req.body);
-  });
+      // res.send(req.body);
+    }
+  );
 
-  app.delete("/api/users/:id", requireLogin, (req, res) => {
-    User.findOneAndDelete({ _id: req.params.id }, req.body, (err, data) => {
-      if (!err) {
-        res.send({ message: "deleted" });
-      } else {
-        res.send(err);
-      }
-    });
-  });
+  app.delete(
+    "/api/users/:id",
+    requireLogin,
+    requirePermissions(["DELETE_USERS"]),
+    (req, res) => {
+      User.findOneAndDelete({ _id: req.params.id }, req.body, (err, data) => {
+        if (!err) {
+          res.send({ message: "deleted" });
+        } else {
+          res.send(err);
+        }
+      });
+    }
+  );
 
   // PUBLIC
   app.get("/api/users/:id", (req, res) => {
